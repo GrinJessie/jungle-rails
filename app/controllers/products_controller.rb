76f1168d8553e47938a9d061893ada006a3bc1e0
GrinJessie <lijing@ualberta.ca>
2018-03-05 01:03:33 +0000

    display average rating on home page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,9 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all.order(created_at: :desc)
+    @products= Product.all.order(created_at: :desc)
   end
+
 
   def show
     @product = Product.find params[:id]
